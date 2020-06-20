ad243a78adc9501366eb809a1711fa385800d846
arisvanmazijk <arisvanmazijk@hotmail.com>
2020-06-20 09:10:01 +0000

    Check on available seats per flight before navigating to Booking view

--- controller/Flights.controller.js
+++ controller/Flights.controller.js
@@ -3,8 +3,9 @@ sap.ui.define([
    "sap/ui/core/UIComponent",
    "sap/ui/core/routing/History",
    "sap/ui/model/Filter",
-   "sap/ui/model/FilterOperator"
-], function (Controller, UIComponent, History, Filter, FilterOperator) {
+   "sap/ui/model/FilterOperator",
+   "sap/m/MessageToast"
+], function (Controller, UIComponent, History, Filter, FilterOperator, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.softwaris.flightapp.controller.Detail", {
 		onInit : function () {
@@ -17,8 +18,10 @@ sap.ui.define([
 			var carrid = window.decodeURIComponent(oEvent.getParameter("arguments").carrid);
 			var connid = window.decodeURIComponent(oEvent.getParameter("arguments").connid);
 			
-			aFilter.push(new Filter("CARRID", FilterOperator.Contains, carrid));
-			aFilter.push(new Filter("CONNID", FilterOperator.Contains, connid));
+			if (carrid && connid) {
+				aFilter.push(new Filter("CARRID", FilterOperator.Contains, carrid));
+				aFilter.push(new Filter("CONNID", FilterOperator.Contains, connid));
+			}
 			
 			var oList = this.byId("flightsList");
 			var oBinding = oList.getBinding("items");
@@ -37,9 +40,27 @@ sap.ui.define([
 				}
 			},
 			
-		navToBooking: function () {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("booking");
-		}			
+		navToBooking: function (oEvent) {
+				var oItem = oEvent.getSource().getBindingContext("flights");
+				if (this._validateSeats(oItem) === true) {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("booking");
+				} else {
+					var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+					MessageToast.show(oResourceBundle.getText("messageToastText"));
+				}
+		},
+		
+		_validateSeats: function (oItem) {
+			var economy = oItem.getObject("SEATSMAX") - oItem.getObject("SEATSOCC");
+			var business = oItem.getObject("SEATSMAX_B") - oItem.getObject("SEATSOCC_B");
+			var first = oItem.getObject("SEATSMAX_F") - oItem.getObject("SEATSOCC_F");
+			
+			if (economy === 0 && business === 0 && first === 0) {
+				return false;
+			} else {
+				return true;
+			}
+		}
 	});
 });
